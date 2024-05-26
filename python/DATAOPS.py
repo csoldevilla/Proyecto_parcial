6e89c03879af48b7fa434b1a47e06435000facc5
Christian Soldevilla Andrade - Personal <csoldev@gmail.com>
2024-05-26 18:21:32 -0500

    1era carga

---
+++ python/DATAOPS.py
@@ -0,0 +1,43 @@
+import os
+import warnings
+
+warnings.filterwarnings("ignore")
+
+### Carpeta Dataset ####
+location =  'C:/Users/Christhian/Documents/Cursos/DATA_ANALYTICS_CERTUS/3_20241763DATAOPSMA/proyecto_parcial/python/dataset'
+print(location)
+
+### validar si existe carpeta ###
+if  not os.path.exists(location) : ### si la carpeta no existe
+    ### creo la carpeta
+    os.mkdir(location)
+else: ### si la carpeta existe
+    ### borrar contenido carpeta
+    for root, dirs , files in  os.walk(location,topdown=False):
+        for name in files:
+            os.remove(os.path.join(root,name)) ### eliminar archivos de la carpeta
+        for name in dirs:
+            os.rmdir(os.path.join(root,name)) ### eliminar la carpeta despues de estar vacio
+
+### imprtar libreria api kaggle ###
+from  kaggle.api.kaggle_api_extended import KaggleApi
+
+api = KaggleApi()
+api.authenticate()
+
+### Listar dataset disponibles ###
+#print(api.dataset_list(search=''))
+
+### Descargar Dataset ###
+
+dataset_name = 'rahulvyasm/netflix-movies-and-tv-shows'
+file_name    = 'netflix_titles.csv'
+path_name    = location
+
+api.dataset_download_file(dataset_name,file_name,path = path_name)
+#api.dataset_download_file(dataset_name,file_name,path = path_name,force=True,quiet=False)
+#api.dataset_download_file(dataset_name,file_name,path = location ,force=True,quiet=False)
+
+api.dataset_download_files('rahulvyasm/netflix-movies-and-tv-shows',path=path_name,force=True,quiet=False,unzip=True)
+
+
---
+++ sql/Carga_datos.sql
@@ -0,0 +1,33 @@
+use Kaggle
+-- en caso no exista la tabla netflix
+if not exists (select  * from sys.tables where object_id = object_id(N'dbo.netflix') and TYPE='U')   
+ --drop table netflix
+create table netflix (
+[show_id] varchar(max),
+[type_show] varchar(max),
+[title] varchar(max),
+[director] varchar(max),
+[cast_show] varchar(max),
+[country] varchar(max),
+[date_added] varchar(max),
+[release_year] varchar(max),
+[rating] varchar(max),
+[duration] varchar(max),
+[listed_in] varchar(max),
+[description] varchar(max)
+)
+-- si la tabl aya existe entonces la trunco
+TRUNCATE TABLE dbo.netflix
+
+-- ingresar dataset
+BULK INSERT dbo.netflix
+from 'C:\Users\Christhian\Documents\Cursos\DATA_ANALYTICS_CERTUS\3_20241763DATAOPSMA\proyecto_parcial\python\dataset\netflix_titles.csv'
+with
+(
+FIRSTROW = 2,			-- empieza en la 2da fila, ya que la primera fila son rotulos
+FIELDTERMINATOR =',',	-- indicamos el separador de columnas(delimitador),
+ROWTERMINATOR ='0x0a'	-- hace referencia a un salto de linea
+)
+Go
+
+select  * from netflix
